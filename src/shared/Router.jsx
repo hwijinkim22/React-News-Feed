108e4d59a237a394cafc9fd7cbf4ea914c9f358d
김휘진 <kim@KIM-hwi-jin.local>
2024-05-31 14:35:25 +0900

    Router-Dom 초기 세팅

---
+++ src/shared/Router.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+import MyPage from '../pages/MyPage'
+
+const Router = () => {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="mypage" element={<MyPage/>} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default Router
