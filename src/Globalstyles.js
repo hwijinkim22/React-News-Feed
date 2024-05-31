8328cf0bb24e7ce569acf423532883e5f6938d39
hyorimhan <151856914+hyorimhan@users.noreply.github.com>
2024-05-31 23:29:49 +0900

    Update Globalstyles.js

--- src/Globalstyles.js
+++ src/Globalstyles.js
@@ -5,8 +5,14 @@ const GlobalStyles = createGlobalStyle`
   max-width: 1280px;
   width: 100%;
   margin: 0 auto;
-  padding: 2rem;
-  font-family: Arial, sans-serif;
+  
+
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
   }
   
 `;
