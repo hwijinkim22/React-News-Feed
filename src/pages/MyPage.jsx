e3f9e312e632916b1a6668ebf93a0491bf0775fb
hyorimhan <mrahn102@gmail.com>
2024-05-31 20:50:15 +0900

    마이 페이지 틀

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,81 @@
-import React from 'react';
+// import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const MyPageDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 23px;
+  border-bottom: 1px solid gray;
+  padding: 10px;
+`;
+
+const Profiles = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  margin-top: 50px;
+`;
+
+const Profile = styled.span`
+  padding: 5px;
+`;
+
+// const List = styled.div`
+//   border-bottom: 1px solid gray;
+//   margin: 100px;
+// `;
+const Notes = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: space-between;
+  margin: 100px auto;
+`;
+const Note = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  height: 250px;
+  background-color: #d9d9d9;
+`;
 const MyPage = () => {
-  return <div>MyPage</div>;
+  // const { myPageId } = useParams;
+
+  return (
+    <Container>
+      <MyPageDiv>마이 페이지</MyPageDiv>
+
+      <Profiles>
+        <Profile>
+          <img src="../../public/iconmonstr-user-circle-thin-48.png" alt="" />
+        </Profile>
+        <Profile>LunaBloom</Profile>
+        <Profile>✒️</Profile>
+      </Profiles>
+
+      {/* <List></List> */}
+
+      <Notes>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </Notes>
+    </Container>
+  );
 };
 
 export default MyPage;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '../pages/Home'
-import MyPage from '../pages/MyPage'
-import CommitDetail from '../pages/CommitDetail'
-import DetailPage from '../pages/DetailPage'
-import Test from '../components/Test'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import MyPage from '../pages/MyPage';
+import CommitDetail from '../pages/CommitDetail';
+import DetailPage from '../pages/DetailPage';
+import Test from '../components/Test';
 
 const Router = ({ posts }) => {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/mypage/:id" element={<MyPage/>} />
-        <Route path="/commitdetail" element={<CommitDetail/>} />
-        <Route path="/detailpage" element={<DetailPage/>} />
-        <Route path="/test" element={<Test posts={posts} />} />
-
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mypage/:mypageId" element={<MyPage posts={posts} />} />
+          <Route path="/commitdetail" element={<CommitDetail />} />
+          <Route path="/detailpage" element={<DetailPage />} />
+          <Route path="/test" element={<Test posts={posts} />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
