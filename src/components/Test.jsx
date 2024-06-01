7711eab2b49f4e2b22b8d77acd3d55e445292638
Captain-Kim <brighthero@kakao.com>
2024-06-01 15:45:57 +0900

    fix: posts 렌더링 개행 문제 수정(인라인 작성)

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -61,7 +61,7 @@ const Test = ({ posts }) => {
         <PostCard key={post.id}>
           <h5>글 제목: {post.title}</h5>
           <h5>닉네임: {currentUser?.user_metadata?.name || post.display_name}</h5>
-          <h5>글 내용: {post.content}</h5>
+          <p style={{whiteSpace:"pre-wrap"}}>글 내용: {post.content}</p>
           {currentUser && currentUser.id === post.user_id && (
             <div>
               <Button onClick={() => handleEdit(post)}>수정</Button>
