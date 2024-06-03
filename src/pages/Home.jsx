ae224b9087d9fefa9b4d645c71fb5d5953b4e58e
김휘진 <kim@KIM-hwi-jin.local>
2024-06-03 11:24:27 +0900

    Fix Login Logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleLoginPage = () => {
+    navigate('/login')
+  }
   return (
     <>
     <h1>Feed</h1>
     <h1>MyPage</h1>
-    <button>로그인 버튼</button>
+    <button onClick={handleLoginPage}>로그인 버튼</button>
     </>
 
     
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,84 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createClient } from '@supabase/supabase-js';
+import styled from 'styled-components';
 
-const supabase = createClient("https://nozekgjgeindgyulfapu.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vemVrZ2pnZWluZGd5dWxmYXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxNDI1MDEsImV4cCI6MjAzMjcxODUwMX0.Wu1dt8WSMSro-_ieydr-ghmfcKPr568Ovm6dfzgrB00");
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 500px;
+  margin: 50px auto;
+  border: 5px solid #4854847c;
+  box-sizing: border-box;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  margin-bottom: 15px;
+  padding: 8px;
+  width: 200px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const Button = styled.button`
+  margin: 5px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const supabase = createClient(
+  'https://nozekgjgeindgyulfapu.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vemVrZ2pnZWluZGd5dWxmYXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxNDI1MDEsImV4cCI6MjAzMjcxODUwMX0.Wu1dt8WSMSro-_ieydr-ghmfcKPr568Ovm6dfzgrB00'
+);
 
 const LoginPage = () => {
   const navigate = useNavigate();
 
   async function signInWithGithub() {
-    await supabase.auth.signInWithOAuth({
-      provider: 'github',
+    const { error, data } = await supabase.auth.signInWithOAuth({
+      provider: 'github'
     });
+
+    if (error) {
+      console.error('깃허브 로그인 에러', error.message);
+      alert('로그인 오류가 발생하였습니다, 다시 시도해주세요.');
+    }
+
+    const { user } = data;
+    if (!user) {
+      alert('회원가입이 필요합니다.');
+      navigate('/signup');
+    }
   }
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
       if (session) {
-        navigate('/');
+        // navigate('/');
       }
     });
 
@@ -26,11 +88,17 @@ const LoginPage = () => {
   }, [navigate]);
 
   return (
-    <div>
-      <h2>로그인 페이지</h2>
-      <button onClick={signInWithGithub}>GitHub로 로그인</button>
-      <button onClick={() => navigate('/signup')}>회원가입</button>
-    </div>
+    <Container>
+      <Title>로그인 페이지</Title>
+      <Form>
+        <Label htmlFor="email">아이디:</Label>
+        <Input type="text" id="email" name="email" />
+        <Label htmlFor="password">비밀번호:</Label>
+        <Input type="text" id="password" name="password" />
+      </Form>
+      <Button onClick={signInWithGithub}>GitHub로 로그인</Button>
+      <Button onClick={() => navigate('/signup')}>회원가입</Button>
+    </Container>
   );
 };
 
