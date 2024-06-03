b0bd2c9fa33688f88184ae3635afc78beef77d13
hoondolla <gnswo9187@daum.net>
2024-06-03 13:45:18 +0900

    더보기, 닫기 추가

--- src/components/HomeFeed.jsx
+++ src/components/HomeFeed.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -13,7 +13,8 @@ const HomeContent = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   gap: 10px;
-  border: 5px solid black;
+  border: 8px solid #343434;
+  border-radius: 15px;
 `;
 
 const HomePostContent = styled.div`
@@ -32,10 +33,36 @@ const HomePost = styled.div`
     box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
   }
   cursor: pointer;
+  border-radius: 5px;
+`;
+
+const MoreButton = styled.button`
+  margin-top: 5px;
+  margin-right: 10px;
+  margin-left: 600px;
+  padding: 10px 20px;
+  background-color: #343434;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  display: flex;
+`;
+
+const CloseButton = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  margin-left: 600px;
+  background-color: #343434;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 const HomeFeed = ({ posts }) => {
   const navigate = useNavigate();
+  const [showAll, setShowAll] = useState(false);
 
   const moveDetailPage = () => {
     navigate('/detailpage');
@@ -44,7 +71,7 @@ const HomeFeed = ({ posts }) => {
   return (
     <div>
       <HomeContent>
-        {posts.map((post) => (
+        {posts.slice(0, showAll ? posts.length : 9).map((post) => (
           <HomePost key={post.id} onClick={moveDetailPage}>
             <h5>글 제목 : {post.title}</h5>
             <h5>닉네임 : {post.nickname}</h5>
@@ -52,6 +79,11 @@ const HomeFeed = ({ posts }) => {
           </HomePost>
         ))}
       </HomeContent>
+      {showAll ? (
+        <CloseButton onClick={() => setShowAll(false)}>닫기</CloseButton>
+      ) : (
+        <MoreButton onClick={() => setShowAll(true)}>더보기</MoreButton>
+      )}
     </div>
   );
 };
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Test = ({ posts }) => {
   return (
     <div>
       <h3>데이터 fetch 테스트 페이지입니다.</h3>
       {posts.map((post) => (
-        <div key={post.id} style={{ border: "1px solid black", marginBottom: "10px" }}>
+        <div key={post.id} style={{ border: '1px solid black', marginBottom: '10px' }}>
           <h5>글 제목 : {post.title}</h5>
           <h5>닉네임 : {post.nickname}</h5>
           <h5>글 내용 : {post.content}</h5>
@@ -14,5 +14,4 @@ const Test = ({ posts }) => {
     </div>
   );
 };
-
 export default Test;
